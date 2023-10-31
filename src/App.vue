aed72fff01352ddc4a8dd476770478257f7ed9d2
milkcoco <lin0968329658@gmail.com>
2023-10-31 15:49:10 +0800

    10/31

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import English  from "./components/EnglishDB.vue";
 import History from "./components/HistoryDB.vue";
 import Geography from "./components/GeographyDB.vue";
 import Biology from "./components/BiologyDB.vue"
-import { reactive } from "vue";
-let choice = reactive({title:'英文', value:"English"});
+import { reactive, ref } from "vue";
+let drawer = ref(false);
+let choice = reactive({title:'國文', value:"Chinese"});
 let items=[
   {title:'國文', value:"Chinese"},
   {title:'英文', value:"English"},
@@ -16,15 +17,33 @@ let items=[
 </script>
 
 <template>
-  <div style="width: 25rem">
-    <v-select label="請選擇" v-model="choice.value" :items="items" item-title="title" item-value="value">
-    </v-select>
-    <Suspense>
-      <Chinese v-if="choice.value === 'Chinese'" />
-      <English v-else-if="choice.value === 'English'" />
-      <History v-else-if="choice.value === 'History'" />
-      <Geography v-else-if="choice.value === 'Geography'" />
-      <Biology v-else />
+  <v-layout class="rounded rounded-md">
+    <v-app-bar>    
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">menu</v-app-bar-nav-icon>
+      <v-app-bar-title>Application bar</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer floating permanent v-model="drawer">
+      <v-list>
+        <v-list-item v-for="item in items"
+          :title="item.title" 
+          :key="item.value" 
+          :active="item.value === choice.value"
+          @click="choice.value = item.value">
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex flex-column align-start justify-start mb-6" style="min-height: 300px;">
+      <!-- <v-select label="請選擇" v-model="choice.value" :items="items">
+      </v-select> -->
+      <Suspense>
+        <Chinese v-if="choice.value === 'Chinese'" />
+        <English v-else-if="choice.value === 'English'" />
+        <History v-else-if="choice.value === 'History'" />
+        <Geography v-else-if="choice.value === 'Geography'" />
+        <Biology v-else />
     </Suspense>
-  </div>
+    </v-main>
+  </v-layout>
 </template>
+
--- src/components/BiologyDB.vue
+++ src/components/BiologyDB.vue
@@ -1,36 +1,74 @@
 <script setup lang="ts">
-import { reactive } from "vue";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { reactive, watch } from "vue";
+import { collection, getDocs, getFirestore, where, query } from "firebase/firestore";
 import app from "@/components/settings/FirebaseConfig.vue";
-const db = getFirestore(app);
-const querySnapshot = await getDocs(collection(db, "Biology"));
-let exams:{question:string, answer:string}[]=[];
-querySnapshot.forEach((doc) => {
-  exams.push({question:doc.data().question, answer:doc.data().answer});
-  console.log('${doc.id} => ${doc.data()}');
-});
-let answer = "";
-const state = reactive({ message: "", currentQuestion: 0 });
 
-function generateQuestion() {
-  if (exams[state.currentQuestion].answer === answer) {
-    state.message = "答案正確";
-    if (state.currentQuestion + 1 < exams.length) {
-      state.currentQuestion++;
+let units=[
+  {title:'單元一 植物', value:1},
+  {title:'單元二 動物', value:2},
+]
+const state = reactive({choice:{title:'單元一', value:1}, answer:[''], message: [''], exams: [{question:'', answer:''}] });
+
+
+async function generateQuestions(){
+  console.log(state.choice)
+  state.exams=[];
+  const queryExam = query(examCollection, where("unit", "==", state.choice));
+  const querySnapshot = await getDocs(queryExam);
+  querySnapshot.forEach((doc) => {
+    state.exams.push({question:doc.data().question, answer:doc.data().answer});
+  });
+}
+
+const db = getFirestore(app);
+const examCollection = collection(db, "Biology");
+generateQuestions();
+watch(() => state.choice,generateQuestions);
+
+// const querySnapshot = await getDocs(queryExam);
+
+// let exams:{question:string, answer:string}[]=[];
+// querySnapshot.forEach((doc) => {
+//   exams.push({question:doc.data().question, answer:doc.data().answer});
+//   console.log('${doc.id} => ${doc.data()}');
+// });
+
+// let answer = "";
+// const state = reactive({ message: "", currentQuestion: 0 });
+// function generateQuestion() {
+//   if (exams[state.currentQuestion].answer === answer) {
+//     state.message = "答案正確";
+//     if (state.currentQuestion + 1 < exams.length) {
+//       state.currentQuestion++;
+//     }
+//   } else {
+//     state.message = "答案錯誤";
+//   }
+// }
+// generateQuestion();
+
+function checkAnswers() {
+  state.message = []; // clear previous messages
+  for (let i in state.exams) {
+    if (state.answer[i] !== state.exams[i].answer) {
+      state.message[i]="不正確";
     }
-  } else {
-    state.message = "答案錯誤";
+    else {
+      state.message[i]="正確";}
   }
 
 }
-generateQuestion();
+
 </script>
 <template>
-  <div>
-    <v-text-field v-model="answer"
-      :label="exams[state.currentQuestion].question"
-      :messages="state.message">
-    </v-text-field>
-    <v-btn color="primary" @click="generateQuestion">下一題</v-btn>
-  </div>
+  <v-container>
+    <v-select label="請選擇" v-model="state.choice" :items="units">
+    </v-select>
+    <div v-for="(exam, index) in state.exams" :key="index">
+      <v-text-field v-model="state.answer[index]" 
+      :label="exam.question" 
+      :messages="state.message[index]"></v-text-field>
+    </div>
+    <v-btn color="primary" @click="checkAnswers">檢查答案</v-btn>
+  </v-container>
 </template>
