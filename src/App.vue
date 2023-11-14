6f58d1fafee143859c633696cfb18563f4b1f65f
milkcoco <lin0968329658@gmail.com>
2023-11-14 15:59:11 +0800

    11/14 complete

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { provide, reactive, readonly, ref } from 'vue'
 import app from '@/components/settings/FirebaseConfig.vue'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import router from './router';
 
 let drawer = ref(false)
 let items = [
@@ -9,7 +10,7 @@ let items = [
   { title: '英文', to: '/english' },
   { title: '地理', to: '/geography' },
   { title: '歷史', to: '/history' },
-  { title: '生物', to: '/biology' }
+  { title: '生物測驗', to: '/biology' }
 ]
 
 const account = reactive({
@@ -35,6 +36,9 @@ const unsub = onAuthStateChanged(auth, (user)=>{
 
 provide(/* key */ 'account', /* value */ readonly(account))
 
+function goAccount(){
+  router.push('/account')
+}
 </script>
 
 <template>
@@ -42,7 +46,10 @@ provide(/* key */ 'account', /* value */ readonly(account))
     <v-app-bar>
       <v-icon icon="plus"></v-icon>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer">menu</v-app-bar-nav-icon>
-      <v-app-bar-title>Application bar  {{ account.email }}</v-app-bar-title>
+      <v-app-bar-title>Application bar</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined" v-if="account.email === ''" @click="goAccount">請先登入</v-btn>
+      <v-btn variant="outlined" v-else-if="account.email != ''" @click="goAccount">登出</v-btn>
     </v-app-bar>
     <v-navigation-drawer floating permanent v-model="drawer">
       <v-list>
@@ -54,11 +61,6 @@ provide(/* key */ 'account', /* value */ readonly(account))
     <v-main class="d-flex flex-column align-start justify-start mb-6" style="min-height: 300px">
       <Suspense>
         <RouterView />
-        <!-- <English v-if="choice.value === 'English'" />
-        <Biology v-else-if="choice.value === 'Biology'"/>
-        <History v-else-if="choice.value === 'History'"/>
-        <Geography v-else-if="choice.value === 'Geography'"/>
-        <Chinese v-else /> -->
       </Suspense>
     </v-main>
   </v-app>
